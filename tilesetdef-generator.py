55f98806a39030e94864f9d896b429549af202a7
Justin Jacobs <jajdorkster@gmail.com>
2013-05-12 08:25:17 -0400

    init

---
+++ tilesetdef-generator.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python2
+import os, sys
+import xml.etree.ElementTree as ET
+
+def main():
+	tileset_w = 0
+	tileset_h = 0
+	start_index = 0
+	end_index = 1
+	tileset_offset = 0
+
+	# returns a formated line defining a tile
+	def getTile(index, w, h, off_x, off_y):
+		tiles_per_row = tileset_w / w
+		true_index = index - start_index
+		left_x = (true_index % tiles_per_row) * w
+		top_y = ((true_index / tiles_per_row) * h) + tileset_offset
+		offset_x = (map_w / 2) - off_x
+		offset_y = h - (map_h /2) - off_y
+		return "tile=%d,%d,%d,%d,%d,%d,%d\n" % (index,left_x,top_y,w,h,offset_x,offset_y)
+
+	# load the TMX file
+	tiled_file = ""
+	for opt in sys.argv:
+		if (opt ==  sys.argv[0]):
+			continue
+		if (os.path.isfile(opt)):
+			tiled_file = opt
+			break
+
+	if (tiled_file == ""):
+		print "No Tiled TMX file specified"
+		return
+
+	parser = ET.parse(tiled_file)
+	root = parser.getroot()
+
+	# get the overall tilesize
+	map_w = int(root.get('tilewidth'))
+	map_h = int(root.get('tileheight'))
+
+	# get the tileset filename
+	map_name = "output"
+	for prop in root.find('properties').findall('property'):
+		if (prop.get('name') == "tileset"):
+			map_name = os.path.splitext(prop.get('value'))[0]
+	map_image_name = map_name + ".png"
+	map_name += ".txt"
+
+	image_files = ""
+
+	# delete previously generated image sheet
+	if (os.path.isfile(map_image_name)):
+		os.remove(map_image_name)
+
+	# create the tilesetdef file
+	map_file = open(map_name,'w')
+
+	# add the tileset filename
+	map_file.write("# %s\n\n" % (map_name))
+	map_file.write("img=%s\n\n" % (map_image_name))
+
+	# add each tile
+	for tileset in root.findall('tileset'):
+		tile_w = int(tileset.get('tilewidth'))
+		tile_h = int(tileset.get('tileheight'))
+		tileset_name = tileset.get('name')
+
+		tileset_w = 0
+		tileset_h = 0
+		image = tileset.find('image')
+		tileset_image = image.get('source')
+		tileset_w = int(image.get('width'))
+		tileset_h = int(image.get('height'))
+
+		tileoffset_x = 0
+		tileoffset_y = 0
+		tileoffset = tileset.find('tileoffset')
+		if (tileoffset != None):
+			tileoffset_x = int(tileoffset.get('x'))
+			tileoffset_y = int(tileoffset.get('y'))
+
+		start_index = end_index
+		end_index += (tileset_w/tile_w) * (tileset_h/tile_h)
+		if (tileset_name != "collision" and tileset_name != "set_rules"):
+			map_file.write("# " + tileset_name + "\n")
+			for i in range(start_index,end_index):
+				map_file.write(getTile(i,tile_w,tile_h,tileoffset_x,tileoffset_y))
+			tileset_offset += tileset_h
+			map_file.write("\n\n")
+			image_files += tileset_image + " "
+
+	map_file.close()
+
+	# combine all tileset images into one
+	os.system("convert "+image_files+" -background transparent -append "+map_image_name)
+
+if __name__ == "__main__":
+	main()
